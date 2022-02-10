3703da31f446233b39c7c0fd0a154d383c06345b
Olesia Yurchenko <olesya.yurchenko91@gmail.com>
2022-02-11 01:11:44 +0200

    js part

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Profile from "./Profile/Profile";
+import user from ""
+
+export const App = () => {
+  return (
+    <div>
+      <Profile/>
+    </div>
+  );
+};
+
+
---
+++ src/components/FriendList/FriendList.js
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+import FriendListItem from './FriendListItem';
+
+const FriendList = ({ friends }) => {
+    return (
+        <ul class="friend-list">
+            {friends.map(({ id, avatar, name, isOnline}) => (
+                <FriendListItem
+                    key={id}
+                    avatar={avatar}
+                    name={name}
+                    isOnline={isOnline}
+                />
+  ))}
+</ul>
+    )
+}
+
+FriendList.propTypes = {
+    friends: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+}))
+}
+
+export default FriendList;
